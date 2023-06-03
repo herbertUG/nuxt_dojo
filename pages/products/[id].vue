76fe88d552b4f6dbe0dc9686f331b387b6faa324
herbertug <herbertoketayot@gmail.com>
2023-06-03 09:52:05 +0300

    fetch data from fake backend

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio id voluptatem quod et tempora odit nesciunt
-      nisi, iusto, rerum ipsum nemo! Natus ea a voluptates accusamus voluptate, adipisci quia qui.</p>
+    <p>{{ product.titile }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params
+const uri = 'https://fakestoreapi.com/products/' + id
+
+const { data: product } = await useFetch(uri, { key: id })
+
 definePageMeta({
   layout: 'products'
 })
+
 </script>
 
 <style scoped></style>
